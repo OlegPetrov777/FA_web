83edcb3b4cda2650ec47a5f4019a1f2834e22848
Oleg <59148657+OlegPetrov777@users.noreply.github.com>
2022-05-21 18:12:31 +0300

    Some changes

--- WetherBot/weatherBot.js
+++ WetherBot/weatherBot.js
@@ -56,21 +56,20 @@ weatherBot.get('/get/:city', (req, res)=>{
     let city = req.params.city;
     city = cyrillicToTranslit().transform(city, "_");
     const url = `https://pogoda.mail.ru/prognoz/${city}`;
-    // rp(url)
-    //     .then(function(html){
-    //         const $ = cheerio.load(html);
-    //         let data = [];
-    //         $('body > div.g-layout.layout.layout_banner-side.js-module > div:nth-child(2) > div.block.block_forecast.block_index.forecast-rb-bg > div > div.information.block.js-city_one > div.information__content > div.information__content__wrapper.information__content__wrapper_left > a > div.information__content__additional.information__content__additional_first > div:nth-child(1)').each((idx, elem)=>{
-    //             const title = $(elem).text();
-    //             data.push(title);
-    //             res.send(city);
-    //         });
-    //         // $('')
-    //     })
-    //     .catch(function(err){
-    //         //ошибка
-    //         console.log(err);
-    //     })
+    rp(url)
+        .then(function(html){
+            const $ = cheerio.load(html);
+            let data = [];
+            $('body > div.g-layout.layout.layout_banner-side.js-module > div:nth-child(2) > div.block.block_forecast.block_index.forecast-rb-bg > div > div.information.block.js-city_one > div.information__content > div.information__content__wrapper.information__content__wrapper_left > a > div.information__content__additional.information__content__additional_first > div:nth-child(1)').each((idx, elem)=>{
+                const title = $(elem).text();
+                data.push(title);
+                res.send(city);
+            });
+        })
+        .catch(function(err){
+            //ошибка
+            console.log(err);
+        })
     res.send(city);
     
 });
